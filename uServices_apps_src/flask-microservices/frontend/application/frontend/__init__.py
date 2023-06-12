ebad9f87dec4f61c1a2d7d70e714c2c02b24be49
Ashwin Francis <ashwin.francis@gmail.com>
2023-06-12 09:13:21 +0200

    Delete uServices_apps_src/flask-microservices directory

--- uServices_apps_src/flask-microservices/frontend/application/frontend/__init__.py
+++
@@ -1,6 +0,0 @@
-# application/frontend/__init__.py
-from flask import Blueprint
-
-frontend_blueprint = Blueprint('frontend', __name__)
-
-from . import views
--- uServices_apps_src/flask-microservices/product-service/application/product_api/__init__.py
+++
@@ -1,6 +0,0 @@
-# application/product_api/__init__.py
-from flask import Blueprint
-
-product_api_blueprint = Blueprint('product_api', __name__)
-
-from . import routes
